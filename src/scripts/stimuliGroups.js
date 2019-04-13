afbd0c33c66af64ca9c2fdbdc750c39e79880955
Don Sartain <dsartain18@gmail.com>
2019-04-12 23:22:38 -0500

    Split JSON into two files. Added AJAX post to Azure Functions

---
+++ src/scripts/stimuliGroups.js
@@ -0,0 +1,203 @@
+// Order in which we display the stimuli to the user
+let stimuliGroups = [
+    {
+        "id": "1",
+        "experimentStories": [
+            {
+                "experimentId": 1,
+                "stories": [
+                    {
+                        "type": "short",
+                        "storyId": 1
+                    },
+                    {
+                        "type": "filler",
+                        "storyId": 0
+                    },
+                    {
+                        "type": "long",
+                        "storyId": 1
+                    }
+                ]
+            },
+            {
+                "experimentId": 2,
+                "stories": [
+                    {
+                        "type": "fast",
+                        "storyId": 1
+                    },
+                    {
+                        "type": "filler",
+                        "storyId": 0
+                    },
+                    {
+                        "type": "slow",
+                        "storyId": 1
+                    }
+                ]
+            },
+            {
+                "experimentId": 3,
+                "stories": [
+                    {
+                        "type": "easy",
+                        "storyId": 1
+                    },
+                    {
+                        "type": "filler",
+                        "storyId": 0
+                    },
+                    {
+                        "type": "hard",
+                        "storyId": 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": "2",
+        "experimentStories": [
+            {
+                "experimentId": 1,
+                "stories": [
+                    {
+                        "type": "short",
+                        "storyId": 2
+                    },
+                    {
+                        "type": "filler",
+                        "storyId": 0
+                    },
+                    {
+                        "type": "long",
+                        "storyId": 2
+                    }
+                ]
+            },
+            {
+                "experimentId": 2,
+                "stories": [
+                    {
+                        "type": "fast",
+                        "storyId": 2
+                    },
+                    {
+                        "type": "filler",
+                        "storyId": 0
+                    },
+                    {
+                        "type": "slow",
+                        "storyId": 2
+                    }
+                ]
+            },
+            {
+                "experimentId": 3,
+                "stories": [
+                    {
+                        "type": "easy",
+                        "storyId": 2
+                    },
+                    {
+                        "type": "filler",
+                        "storyId": 0
+                    },
+                    {
+                        "type": "hard",
+                        "storyId": 2
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": "3",
+        "experimentStories": [
+            {
+                "experimentId": 1,
+                "stories": [
+                    {
+                        "type": "short",
+                        "storyId": 1
+                    },
+                    {
+                        "type": "long",
+                        "storyId": 2
+                    }
+                ]
+            },
+            {
+                "experimentId": 2,
+                "stories": [
+                    {
+                        "type": "fast",
+                        "storyId": 2
+                    },
+                    {
+                        "type": "slow",
+                        "storyId": 1
+                    }
+                ]
+            },
+            {
+                "experimentId": 3,
+                "stories": [
+                    {
+                        "type": "easy",
+                        "storyId": 1
+                    },
+                    {
+                        "type": "hard",
+                        "storyId": 2
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": "4",
+        "experimentStories": [
+            {
+                "experimentId": 1,
+                "stories": [
+                    {
+                        "type": "short",
+                        "storyId": 2
+                    },
+                    {
+                        "type": "long",
+                        "storyId": 1
+                    }
+                ]
+            },
+            {
+                "experimentId": 2,
+                "stories": [
+                    {
+                        "type": "fast",
+                        "storyId": 1
+                    },
+                    {
+                        "type": "slow",
+                        "storyId": 2
+                    }
+                ]
+            },
+            {
+                "experimentId": 3,
+                "stories": [
+                    {
+                        "type": "easy",
+                        "storyId": 2
+                    },
+                    {
+                        "type": "hard",
+                        "storyId": 1
+                    }
+                ]
+            }
+        ]
+    }
+]
